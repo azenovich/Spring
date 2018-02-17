dc6238697a801506d0faf3d92844b4ac05a4ba07
azenovich <alexander.zenovich@gmail.com>
2018-02-17 15:54:28 +0300

    init spring boot

---
+++ Mvc_Courses/src/main/java/courses/CoursesMvcApplication.java
@@ -0,0 +1,11 @@
+package courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesMvcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoursesMvcApplication.class, args);
+    }
+}
